42cda1cae136841e9695eb962001f4e804eefdce
Leon Wolfgang Rönnpagel <leworoe@codemaschine.de>
2024-10-16 00:39:52 +0200

    Added support for newer ubuntu

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,12 @@
 FROM ubuntu:latest
 
-ENV GDK_BACKEND='broadway'
-ENV BROADWAY_DISPLAY=':5'
-ENV DARK_MODE='false'
-ENV GTK_THEME='Materia'
-ENV BG_GRADIENT="#ddd, #999"
+ENV GDK_BACKEND="broadway"
+ENV BROADWAY_DISPLAY=":5"
+ENV DARK_MODE="false"
 
-RUN apt-get update
-RUN apt-get install -y --no-install-recommends libgtk-3-0 libgtk-3-bin nginx gettext-base tmux wget materia-gtk-theme papirus-icon-theme
+RUN apt-get update && apt-get upgrade -y
+RUN apt-get install -y --no-install-recommends libgtk-3-0 libgtk-3-bin nginx gettext-base tmux wget materia-gtk-theme papirus-icon-theme gnome-icon-theme
 RUN apt-get clean && apt-get autoclean && rm -rf /var/lib/apt/lists/*
-
-RUN rm -rf /usr/share/themes/Materia && mv /usr/share/themes/Materia-light /usr/share/themes/Materia
 
 RUN wget --no-check-certificate -O /usr/bin/ttyd "https://github.com/tsl0922/ttyd/releases/latest/download/ttyd.$(uname -m)"
 RUN chmod +x /usr/bin/ttyd
--- start.sh
+++ start.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 if [ $DARK_MODE = true ] ; then
-  export BG_GRADIENT="#111, #222"
-  export GTK_THEME="Materia:dark"
   export TERM_COLOR="filter: invert(14%) sepia(7%) saturate(14%) hue-rotate(16deg) brightness(102%) contrast(90%);"
-  cp -r /usr/share/icons/Papirus-Dark /usr/share/icons/Yaru
-else 
-  cp -r /usr/share/icons/Papirus /usr/share/icons/Yaru
+  export GTK_THEME="Materia-dark"
+  export BG_GRADIENT="#111, #222"
+  rm -rf /usr/share/icons/hicolor && ln -s Papirus-Dark /usr/share/icons/hicolor
+else
+  export TERM_COLOR=""
+  export GTK_THEME="Materia-light"
+  export BG_GRADIENT="#ddd, #999"
+  rm -rf /usr/share/icons/hicolor && ln -s Papirus-Light /usr/share/icons/hicolor
 fi
 envsubst '$$BG_GRADIENT $$FAVICON_URL $$APP_TITLE $$CORNER_IMAGE_URL $$TERM_COLOR' < /etc/nginx/nginx.tmpl > /etc/nginx/sites-enabled/default && nginx -g 'daemon off;' &
 nohup /usr/bin/broadwayd :5 &> /var/log/broadway.log &
 tmux new-session -d -s "ttyd" "tmux set -g status off && /bin/bash"
-ttyd tmux a -t ttyd &
+ttyd -W tmux a -t ttyd &
